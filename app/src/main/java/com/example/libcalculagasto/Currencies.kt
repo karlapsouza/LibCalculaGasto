681375888cad233cbd56c94eeea4200f143f2369
karlapsouza <karlapsouza@hotmail.com>
2020-09-16 10:08:50 -0300

    Update Currencies.kt

--- app/src/main/java/com/example/libcalculagasto/Currencies.kt
+++ app/src/main/java/com/example/libcalculagasto/Currencies.kt
@@ -3,10 +3,6 @@ package com.example.libcalculagasto
 import com.google.gson.annotations.SerializedName
 
 data class Currencies(
-    @SerializedName("code")
-    var originalCurrency : String,
-    @SerializedName("codein")
-    var finalCurrency : String,
     @SerializedName("name")
     var name : String,
     @SerializedName("bid")
