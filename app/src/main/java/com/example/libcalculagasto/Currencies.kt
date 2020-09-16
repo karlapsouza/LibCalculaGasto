b47d3d8ca84d7f5c83dc7ce6d9b7f8fefc053754
karlapsouza <karlapsouza@hotmail.com>
2020-09-16 11:16:03 -0300

    Update Currencies.kt

--- app/src/main/java/com/example/libcalculagasto/Currencies.kt
+++ app/src/main/java/com/example/libcalculagasto/Currencies.kt
@@ -3,8 +3,26 @@ package com.example.libcalculagasto
 import com.google.gson.annotations.SerializedName
 
 data class Currencies(
+    @SerializedName("code")
+    var code : String,
+    @SerializedName("codein")
+    var codein : String,
     @SerializedName("name")
     var name : String,
+    @SerializedName("high")
+    var high : Double,
+    @SerializedName("low")
+    var low : Double,
+    @SerializedName("varBid")
+    var varBid : Double,
+    @SerializedName("pctChange")
+    var pctChange : Double,
     @SerializedName("bid")
-    var bid : Double
+    var bid : Double,
+    @SerializedName("ask")
+    var ask : Double,
+    @SerializedName("timestamp")
+    var timestamp : String, 
+    @SerializedName("create_date")
+    var create_date : String 
 )
